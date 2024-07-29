fe6dd0b5eebb589d21a7f8c44f8ef9ce2c87459b
wfxey <158351052+wfxey@users.noreply.github.com>
2024-07-29 21:35:21 +0200

    Update welcome_console_message.js

--- JS/welcome_console_message.js
+++ JS/welcome_console_message.js
@@ -1,6 +1,5 @@
 console.log("This Website is hosted with GHP (GitHub Pages)")
 console.log("Note: This page contains raw information only. There are no external links or redirections, and no downloads or additional content are provided. This is purely raw text.");
 console.log("Made by D&I")
-console.log("Version : v2024.07.23-DI-(4)")
-console.log("THIS WEBSITE IS NOT WORKING PROPERLY, THIS IS JUST A PREVIEW!")
+console.log("Version : v2024.07.29-DI")
 console.log("-------------------------")
